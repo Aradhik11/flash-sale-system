e00c035ef18b8bd65475b4ef572a41b2c600e2a1
Dhikrullah Abdur-Rahmon <aradhik11@gmail.com>
2025-03-05 03:12:21 +0100

    quick fix

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -14,10 +14,10 @@ const options: swaggerJsdoc.Options = {
       }
     },
     servers: [
-      {
-        url: 'https://flash-sale-system-ghj5.onrender.com',
-        description: 'Production server'
-      },
+      // {
+      //   url: 'https://flash-sale-system-ghj5.onrender.com',
+      //   description: 'Production server'
+      // },
       {
         url: 'http://localhost:5000',
         description: 'Development server'
